f7e5a7b5b6329790b6120982e5d9f310996d57d6
vamankrishna <vamankrishna123@gmail.com>
2021-04-17 23:09:05 +0530

    VoterBasic Application

---
+++ VoterApplication/src/com/nt/servlet/VoterServlet.java
@@ -0,0 +1,37 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class VoterServlet extends HttpServlet {
+	
+	
+	public void service(HttpServletRequest req, HttpServletResponse res )throws ServletException,IOException{
+		
+		PrintWriter pw = res.getWriter();
+		res.setContentType("text/html");
+		
+		String name = req.getParameter("name");
+		String age = req.getParameter("age");
+		String address = req.getParameter("address");
+		int nage = Integer.parseInt(age);
+		
+		
+		if(nage>=18)
+			pw.println("<h1 style='color:Green;text-align:center;'>"+name+"You are Eligable for Vote </h1>");
+		else
+			pw.println("<h1 style='color:Green;text-align:center;'>"+name+"You are not  Eligable for Vote </h1>");
+		
+		pw.println("<h1><a href='pages/form.html'><img src='images/house.png'> </a></h1>");
+		
+		pw.close();
+		
+			
+	}
+
+}
